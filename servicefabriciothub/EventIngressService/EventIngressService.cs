f7358a23a99a9a4217d4f33f0f313bc763b1385e
Michaël Fery <michael.fery@outlook.com>
2018-04-28 09:36:42 +0200

    add event hub partition receivers

--- servicefabriciothub/EventIngressService/EventIngressService.cs
+++ servicefabriciothub/EventIngressService/EventIngressService.cs
@@ -8,6 +8,8 @@ using Microsoft.ServiceFabric.Services.Communication.Runtime;
 using Microsoft.ServiceFabric.Services.Runtime;
 using Microsoft.Azure.EventHubs;
 using Microsoft.Azure.ServiceBus;
+using Microsoft.ServiceFabric.Data;
+using Microsoft.ServiceFabric.Data.Collections;
 using TransportType = Microsoft.Azure.ServiceBus.TransportType;
 
 
@@ -18,6 +20,8 @@ namespace EventIngressService
     /// </summary>
     internal sealed class EventIngressService : StatefulService
     {
+        private const string OffsetDictionaryName = "OffsetDictionary";
+
         public EventIngressService(StatefulServiceContext context)
             : base(context)
         { }
@@ -51,6 +55,14 @@ namespace EventIngressService
 
                 // Get an EventHub client connected to the IOT Hub
                 var eventHubClient = GetAmqpEventHubClient(eventHubCompatibleEndpoint, eventHubCompatibleName);
+
+                // These Reliable Dictionaries are used to keep track of our position in IoT Hub for each partition.
+                // If this service fails over, this will allow it to pick up where it left off in the event stream.
+                IReliableDictionary<string, string> offsetDictionary =
+                    await this.StateManager.GetOrAddAsync<IReliableDictionary<string, string>>(OffsetDictionaryName);
+
+                // Get receivers connected to the partitions of the IOT Hub
+                var eventHubReceivers = await GetEventHubReceiversAsync(eventHubClient, offsetDictionary);
 
                 while (true)
                 {
@@ -120,5 +132,58 @@ namespace EventIngressService
             return EventHubClient.CreateFromConnectionString(connectionStringBuilder.GetEntityConnectionString());
         }
 
+
+        /// <summary>
+        /// Creates a dictionary of EventHubReceiver connected though ServiceBus and partitions
+        /// with AMQP protocol
+        /// </summary>
+        /// <param name="eventHubClient">Event Hub Client</param>
+        /// <param name="offsetDictionary"></param>
+        /// <returns></returns>
+        private async Task<Dictionary<string, PartitionReceiver>> GetEventHubReceiversAsync(EventHubClient eventHubClient, IReliableDictionary<string, string> offsetDictionary)
+        {
+            // Get the IoT Hub partitions IDs
+            string[] partitionIds = (await eventHubClient.GetRuntimeInformationAsync()).PartitionIds;
+
+            var eventHubReceivers = new Dictionary<string, PartitionReceiver>();
+
+            foreach (var eventHubPartitionId in partitionIds)
+            {
+                using (ITransaction tx = this.StateManager.CreateTransaction())
+                {
+                    ConditionalValue<string> offsetResult =
+                        await offsetDictionary.TryGetValueAsync(tx, key: eventHubPartitionId, lockMode: LockMode.Default);
+
+                    PartitionReceiver eventHubReceiver;
+                    if (offsetResult.HasValue)
+                    {
+                        ServiceEventSource.Current.ServiceMessage(
+                            this.Context,
+                            $"RoutingService connecting to IoT Hub partition ID {eventHubPartitionId} with offset {offsetResult.Value}");
+
+                        eventHubReceiver = eventHubClient.CreateReceiver(
+                            PartitionReceiver.DefaultConsumerGroupName,
+                            eventHubPartitionId,
+                            EventPosition.FromOffset(offsetResult.Value)
+                            );
+                    }
+                    else
+                    {
+                        var dateTimeUtc = DateTime.UtcNow;
+                        ServiceEventSource.Current.ServiceMessage(
+                            this.Context,
+                            $"RoutingService connecting to IoT Hub partition ID {eventHubPartitionId} with offset {dateTimeUtc}");
+
+                        eventHubReceiver = eventHubClient.CreateReceiver(
+                            PartitionReceiver.DefaultConsumerGroupName,
+                            eventHubPartitionId,
+                            EventPosition.FromEnqueuedTime(dateTimeUtc)
+                        );
+                    }
+                    eventHubReceivers[eventHubPartitionId] = eventHubReceiver;
+                }
+            }
+            return eventHubReceivers;
+        }
     }
 }
